4414e1c2890a0540004e669720227e429475fc5c
Lucas Coronilla <108490148+unknown-Lucas@users.noreply.github.com>
2023-05-18 21:33:31 +0200

    (ref) unused modules

--- src/app/modules/reader/m.ts
+++ src/app/modules/reader/m.ts
@@ -1,6 +1,4 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { ReaderActionsComponent } from './components/readerActions.component';
 
@@ -8,7 +6,5 @@ export const modules = [
   CommonModule,
   NgOptimizedImage,
   MatProgressBarModule,
-  MatButtonModule,
-  MatIconModule,
   ReaderActionsComponent,
 ];
